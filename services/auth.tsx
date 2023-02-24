a56f156b2a97b9f3dbcb895edd9fdcc2fa903c87
wisnusunadi <barto7773@gmail.com>
2023-02-24 12:47:16 +0700

    api

--- services/auth.tsx
+++ services/auth.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import callAPI from "../config/api";
 import { SignInTypes } from "./data-types";
 const ROOT_API = process.env.NEXT_PUBLIC_API;
 export async function setSignUp(data) {
@@ -12,21 +13,10 @@ export async function setSignUp(data) {
 }
 
 export async function setSignIn(data: SignInTypes) {
-  const URL = "auth/signin";
-  const response = await axios.post(`${ROOT_API}/api/${URL}`, data).catch(err => err.response);
-  ;
-  if (response.status > 300) {
-    const res = {
-      error: true,
-      message: response.data.message,
-      data: null
-    }
-    return res;
-  }
-  const res = {
-    error: false,
-    message: response.data.message,
-    data: response.data.data
-  }
-  return res
+  const url = `${ROOT_API}/api/auth/signin`;
+  return callAPI({
+    url,
+    method: 'POST',
+    data
+  })
 }
